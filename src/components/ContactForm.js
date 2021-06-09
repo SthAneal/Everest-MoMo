9f2ea270a1b6d538ad8963ed95f753a6f1bd6fa4
Anil Shrestha <anilmeronaam@gmail.com>
2021-06-09 22:47:27 +1000

    Update ContactForm.js
    validation and responsive behaviour is added.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {FlexDiv, Title} from '../styles/globalStylesComponent';
 import validation from '../components/validation';
 
 const ContactForm = ()=>{
+    const [formMessage, setFormMessage] = useState(null);
+    const [isValid, setValidation] = useState(false);
 
     const inputHandle = (e)=>{
         const label = e.target.previousElementSibling;
@@ -16,28 +18,54 @@ const ContactForm = ()=>{
         }
         //validation(e);
     }
+
+    const sumbitForm = (e)=>{
+        const validationElement = document.getElementsByClassName('validate');
+        let count = 0;
+
+        for(let i=0; i < validationElement.length; i++){
+            if(!validationElement[i].value){
+                count += 1;
+                validationElement[i].classList.remove('validation-passed');
+                validationElement[i].classList.add('validation-failed');
+            }else{
+                validationElement[i].classList.remove('validation-failed');
+                validationElement[i].classList.add('validation-passed');
+            }
+        }
+        
+        if(count > 0){
+            setFormMessage('Please fill the mandatory fields.');
+            setValidation(false);
+        }else{
+            setFormMessage('Thankyou!!! We have received your message. We will contact you soon.');
+            setValidation(true);
+        }
+    }
+
     return(
         <FlexDiv Width="100%" direction="column" borderRadius="1em" className="contact-form shadow" padding="3em 1em">
+            {formMessage && <FlexDiv margin="0 0 2.5em 0" className={isValid?"validation-success":"validation-fail"} Width="100%" justifyContent="center">{formMessage}</FlexDiv>}
             <FlexDiv Width="100%" direction="column" className="contact-form__label-input" position="relative" padding="2em 1em">
                 <label>Name</label>
-                <input type="text" onMouseLeave={(e)=>inputHandle(e)} validation={{type:'String',minLength:'6'}} required/>
+                <input className="validate" type="text" onMouseLeave={(e)=>inputHandle(e)} required/>
             </FlexDiv>
             <FlexDiv Width="100%" direction="row" flexGrow="1" flexWrap="wrap">
                 <FlexDiv Width="50%" minWidth="300px" direction="column"  className="contact-form__label-input" position="relative"  padding="2em 1em">
                     <label>Email</label>
-                    <input type="email"  onMouseLeave={(e)=>inputHandle(e)} required/>
+                    <input className="validate" type="email"  onMouseLeave={(e)=>inputHandle(e)} required/>
                 </FlexDiv>
                 <FlexDiv Width="50%" minWidth="300px" direction="column"  className="contact-form__label-input" position="relative"  padding="2em 1em">
                     <label>Phone no.</label>
-                    <input type="number" onMouseLeave={(e)=>inputHandle(e)} required/>
+                    <input className="validate" type="number" onMouseLeave={(e)=>inputHandle(e)} required/>
                 </FlexDiv>
             </FlexDiv>
             <FlexDiv Width="100%" direction="column"  className="contact-form__label-input" position="relative"  padding="2em 1em">
                 <label>Message</label>
-                <textarea rows="3" onMouseLeave={(e)=>inputHandle(e)} required></textarea>
+                <textarea className="validate" rows="1" onMouseLeave={(e)=>inputHandle(e)} required></textarea>
             </FlexDiv>
             <FlexDiv Width="100%" justifyContent="center" margin="2em 0 0" padding="2em">
-                <button type="submit" className="submitBtn">Submit</button>
+                <button type="submit" className="submitBtn" onClick={(e)=>sumbitForm(e)}>Submit</button>
             </FlexDiv>
         </FlexDiv>
     )
